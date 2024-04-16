efa1b95739ba87259c72da6bfce35b0480f50515
Mac Tirney <mactirney@gmail.com>
2024-04-15 23:02:17 -0400

    feat: add subrental.js file
    Generate basic data for display on the "index.html" webpage.
    Add "subrental.js" to the "src" folder.

---
+++ Admin Dashboard v1/Dashboard/src/subrental.js
@@ -0,0 +1,20 @@
+const Subrental = [
+    {
+        gearName: 'Robe iFortes',
+        poNumber: 'R-155353',
+        company: 'Felix Lighting',
+        status: 'Pending'
+    },
+    {
+        gearName: 'Robe Esprites',
+        poNumber: 'R-155352',
+        company: 'PRG',
+        status: 'Pending'
+    },
+    {
+        gearName: 'Tetra X',
+        poNumber: 'R-155351',
+        company: 'Main Light',
+        status: 'Paid'
+    },
+]
